719ab08d519219dedfe232267118788e00623149
Caio <caioanzelman@gmail.com>
2024-06-09 16:04:35 -0300

    exceção tratada

--- src/main/java/com/example/todolist/infra/TratarExcecoes.java
+++ src/main/java/com/example/todolist/infra/TratarExcecoes.java
@@ -3,11 +3,11 @@ package com.example.todolist.infra;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class TratarExcecoes extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ExcecaoDto> error404() {
